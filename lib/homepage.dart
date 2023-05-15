6f7f77dbdeac66177f2577d82b8de2b5bf0b65b6
iHamza7 <khanazam3324@gmail.com>
2023-05-15 22:15:17 +0500

    lib changed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('this is text'),
+        title: const Text('this is bar'),
       ),
     );
   }
