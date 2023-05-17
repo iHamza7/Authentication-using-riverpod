71f2a1719533d7de9da217fd13d5aa94ae28da26
iHamza7 <khanazam3324@gmail.com>
2023-05-17 21:29:16 +0500

    lib changed

--- lib/authentication/authenticaton_view.dart
+++ lib/authentication/authenticaton_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../components/animated_button.dart';
 import '../components/auth_switch_button.dart';
 import '../components/text_input_field.dart';
+import 'signin/signin.dart';
+import 'signup/signup.dart';
 
 class AuthenticationView extends StatefulWidget {
   const AuthenticationView({super.key});
@@ -20,36 +22,7 @@ class _AuthenticationViewState extends State<AuthenticationView> {
         children: [
           Padding(
             padding: const EdgeInsets.fromLTRB(16, 250, 16, 0),
-            child: Column(
-              children: [
-                TextInputField(
-                  hintText: 'Email',
-                  onChanged: (_) {},
-                ),
-                TextInputField(
-                  hintText: 'Password',
-                  onChanged: (_) {},
-                ),
-                AnimatedButton(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 18),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: const Color(0XFFF5D973),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: const Text(
-                      'SignIn',
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            child: _showSignIn ? const SignIn() : const SignUp(),
           ),
           AuthSwitchButton(
             onTap: () {
