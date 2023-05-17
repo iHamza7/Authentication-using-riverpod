426a2361cea7a260484163284013a20877134121
iHamza7 <khanazam3324@gmail.com>
2023-05-17 21:26:39 +0500

    lib changed

--- lib/authentication/signin/signin.dart
+++ lib/authentication/signin/signin.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
+import '../../components/animated_button.dart';
+import '../../components/text_input_field.dart';
+
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        TextInputField(
+          hintText: 'Email',
+          onChanged: (_) {},
+        ),
+        TextInputField(
+          hintText: 'Password',
+          onChanged: (_) {},
+        ),
+        AnimatedButton(
+          onTap: () {},
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 18),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: const Color(0XFFF5D973),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Text(
+              'SignIn',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
