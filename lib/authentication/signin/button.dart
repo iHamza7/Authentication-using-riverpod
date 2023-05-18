94b08c1cade277e354cbf5e1e732236485698700
iHamza7 <khanazam3324@gmail.com>
2023-05-18 14:54:52 +0500

    lib changed

---
+++ lib/authentication/signin/button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../../components/animated_button.dart';
+import '../../components/roundedbutton.dart';
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedButton(
+      onTap: () {},
+      child: const RoundedButton(
+        title: 'SignUp',
+      ),
+    );
+  }
+}
