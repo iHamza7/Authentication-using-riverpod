3a41ada8f2eb43aa659da4d1158f563228d0f4f0
iHamza7 <khanazam3324@gmail.com>
2023-05-19 14:39:58 +0500

    lib changed

--- lib/components/animated/top_shape.dart
+++ lib/components/animated/top_shape.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
 
 class TopShape extends CustomClipper<Path> {
+  final AnimationController _controller;
+  TopShape(this._controller);
+  static Widget draw(Color color, String text, AnimationController controller) {
+    return ClipPath(
+      clipper: TopShape(controller),
+      child: ColoredBox(
+        color: color,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16,
+            ),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 34,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Path getClip(Size size) {
     final path = Path();
