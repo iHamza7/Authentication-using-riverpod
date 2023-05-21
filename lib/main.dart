9f0230a6843c7db47d4fa6b44f427ef7e5dc0475
iHamza7 <khanazam3324@gmail.com>
2023-05-21 21:02:48 +0500

    lib changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter',
+      title: 'Flutter auth',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
