f9760e23f83c636dd045d4f3038ba4d192031178
iHamza7 <khanazam3324@gmail.com>
2023-05-21 20:31:34 +0500

    changed

---
+++ lib/authentication/signup/controller/signup_state.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:form_validators/form_validators.dart';
+
+class SignUpState extends Equatable {
+  final Name name;
+  final Email email;
+  final Password password;
+  final FormzSubmissionStatus? status;
+  final String? errormessage;
+
+  const SignUpState({
+    this.name = const Name.pure(),
+    this.email = const Email.pure(),
+    this.password = const Password.pure(),
+    this.status,
+    // this.status = FormzStatus.
+    this.errormessage,
+  });
+
+  @override
+  List<Object?> get props => [
+        name,
+        email,
+        password,
+        status,
+      ];
+
+  SignUpState copyWith({
+    Name? name,
+    Email? email,
+    Password? password,
+    FormzSubmissionStatus? status,
+    String? errormessage,
+  }) {
+    return SignUpState(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      status: status ?? this.status,
+      errormessage: errormessage ?? this.errormessage,
+    );
+  }
+}
