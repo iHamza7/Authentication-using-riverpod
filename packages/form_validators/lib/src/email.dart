6b72cc0cd008fa59d5d9fb07fd6db9890acb6dbf
iHamza7 <khanazam3324@gmail.com>
2023-05-21 15:01:39 +0500

    changed

---
+++ packages/form_validators/lib/src/email.dart
@@ -0,0 +1,34 @@
+import 'package:formz/formz.dart';
+
+enum EmailValidationError { empty, invalid }
+
+const String _kEmailPattern =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+class Email extends FormzInput<String, EmailValidationError> {
+  const Email.pure() : super.pure('');
+  const Email.dirty([String value = '']) : super.dirty(value);
+
+  static final _reg = RegExp(_kEmailPattern);
+
+  @override
+  EmailValidationError? validator(String value) {
+    if (_reg.hasMatch(value)) {
+      return null;
+    } else if (value.isEmpty) {
+      return EmailValidationError.empty;
+    } else {
+      return EmailValidationError.invalid;
+    }
+  }
+
+  static String? showEmailError(EmailValidationError? error) {
+    if (error == EmailValidationError.empty) {
+      return 'Empty Email';
+    } else if (error == EmailValidationError.invalid) {
+      return 'invalid';
+    } else {
+      return null;
+    }
+  }
+}
