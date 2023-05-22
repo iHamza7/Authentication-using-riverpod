1bf127f33bc787632c74e09c92ab179582e3af90
iHamza7 <khanazam3324@gmail.com>
2023-05-22 21:13:12 +0500

    lib changed

--- packages/form_validators/lib/src/name.dart
+++ packages/form_validators/lib/src/name.dart
@@ -20,7 +20,7 @@ class Name extends FormzInput<String, NameValidationError> {
 
   static String? showNameError(NameValidationError? error) {
     if (error == NameValidationError.empty) {
-      return 'EmptyName';
+      return 'Empty Name';
     } else if (error == NameValidationError.invalid) {
       return 'Too Short ';
     } else {
