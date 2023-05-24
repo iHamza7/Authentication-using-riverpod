3cbd201327083e0e8e8e764d2c1bb7518581e6d8
iHamza7 <khanazam3324@gmail.com>
2023-05-24 14:28:31 +0500

    lib changed

--- lib/authentication/signup/email.dart
+++ lib/authentication/signup/email.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 import '../../components/text_input_field.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'controller/signup_controller.dart';
+import 'package:form_validators/form_validators.dart';
 
-class EmailField extends StatelessWidget {
+class EmailField extends ConsumerWidget {
   const EmailField({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final signUpState = ref.watch(signUpProvider);
+    final showError = signUpState.name.isNotValid;
+    final signUpController = ref.read(signUpProvider.notifier);
     return TextInputField(
       hintText: 'Email',
-      onChanged: (_) {},
+      errorText:
+          showError ? Email.showEmailError(signUpState.email.error) : null,
+      onChanged: (email) {
+        signUpController.onEmailChange(email);
+      },
     );
   }
 }
