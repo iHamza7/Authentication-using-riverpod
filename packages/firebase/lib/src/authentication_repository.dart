06cffc9ef757d5bcea36bd27e15a8a72a297b278
iHamza7 <khanazam3324@gmail.com>
2023-05-25 20:36:18 +0500

    lib changed

---
+++ packages/firebase/lib/src/authentication_repository.dart
@@ -0,0 +1,18 @@
+class SignUpWithEmailAndPasswordFailure implements Exception {
+  final String code;
+  const SignUpWithEmailAndPasswordFailure(this.code);
+}
+
+class SignInWithEmailAndPasswordFailure implements Exception {
+  final String code;
+  const SignInWithEmailAndPasswordFailure(this.code);
+}
+
+class ForgotPasswordFailure implements Exception {
+  final String code;
+  const ForgotPasswordFailure(this.code);
+}
+
+class SignInWithGoogleFailure implements Exception {}
+
+class SignOutFailure implements Exception {}
