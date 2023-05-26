78dc59a445d41aa1f77097e5d5d92c4181229fc0
iHamza7 <khanazam3324@gmail.com>
2023-05-26 15:46:20 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,38 @@ import 'package:flutter/material.dart';
 import 'authentication/authenticaton_view.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'authentication/controller/authentication_controller.dart';
+import 'profile/user_profile.dart';
+
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authenticationState = ref.watch(authProvider);
+
+    Widget getHome() {
+      if (authenticationState.status == AuthenticationStatus.authenticated) {
+        return const Profile();
+      } else if (authenticationState.status ==
+          AuthenticationStatus.unauthenticated) {
+        return const AuthenticationView();
+      } else {
+        return const AuthenticationView();
+      }
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter authentication using ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const AuthenticationView(),
+      home: getHome(),
     );
   }
 }
