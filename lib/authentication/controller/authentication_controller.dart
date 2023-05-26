4dcd3fcbd247628b530e631a27ca9be37b5bef16
iHamza7 <khanazam3324@gmail.com>
2023-05-26 15:11:08 +0500

    changed

--- lib/authentication/controller/authentication_controller.dart
+++ lib/authentication/controller/authentication_controller.dart
@@ -1,4 +1,18 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 part 'authentication_state.dart';
+
+class AuthController extends StateNotifier<AuthenticationState> {
+  final AuthenticationRepository _authRepository;
+  AuthController(this._authRepository)
+      : super(const AuthenticationState.unauthenticated()) {
+    _authRepository.user.listen((user) => _onUserChanged(user));
+  }
+  void _onUserChanged(AuthUser user) {
+    if (user.isEmpty) {
+      state = const AuthenticationState.unauthenticated();
+    }
+  }
+}
