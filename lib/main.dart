06feca4a36a0dd4b5b52ab31d8876b0458fc7c5a
iHamza7 <khanazam3324@gmail.com>
2023-05-28 03:20:49 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
-
+// import 'package:stack_trace/stack_trace.dart' as stack_trace;
 import 'authentication/authenticaton_view.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'authentication/controller/authentication_controller.dart';
 import 'profile/user_profile.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
+  // FlutterError.demangleStackTrace = (StackTrace stack) {
+  //   if (stack is stack_trace.Trace) return stack.vmTrace;
+  //   if (stack is stack_trace.Chain) return stack.toTrace().vmTrace;
+  //   return stack;
+  // };
 }
 
 class MyApp extends ConsumerWidget {
