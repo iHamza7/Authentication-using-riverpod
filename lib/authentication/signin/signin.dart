05acdfd6611bc6729c8b40f451da27a09dd6e210
iHamza7 <khanazam3324@gmail.com>
2023-05-28 20:47:35 +0500

    lib changed

--- lib/authentication/signin/signin.dart
+++ lib/authentication/signin/signin.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:form_validators/form_validators.dart';
-// import 'controller/signin_controller.dart';
-// import '../../components/loading_error.dart';
+import 'package:form_validators/form_validators.dart';
+import 'controller/signin_controller.dart';
+import '../../components/loading_error.dart';
 
 import '../google_signin/google_signin_button.dart';
 import 'button.dart';
@@ -16,16 +16,16 @@ class SignIn extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // ref.listen<SignInState>(signInProvider, (previous, current) {
-    //   if (current.status.isInProgress) {
-    //     LoadingSheet.show(context);
-    //   } else if (current.status.isFailure) {
-    //     Navigator.of(context).pop();
-    //     ErrorDialog.show(context, "${current.errorMessage}");
-    //   } else if (current.status.isSuccess) {
-    //     Navigator.of(context).pop();
-    //   }
-    // });
+    ref.listen<SignInState>(signInProvider, (previous, current) {
+      if (current.status.isInProgress) {
+        LoadingSheet.show(context);
+        // } else if (current.status.isFailure) {
+        //   Navigator.of(context).pop();
+        //   ErrorDialog.show(context, "${current.errorMessage}");
+      } else if (current.status.isSuccess) {
+        Navigator.of(context).pop();
+      }
+    });
     return const Column(
       children: [
         EmailField(),
