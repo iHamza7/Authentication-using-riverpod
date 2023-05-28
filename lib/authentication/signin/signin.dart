a4746d10c71e52c1a21059b2d621bbd77c3c9137
iHamza7 <khanazam3324@gmail.com>
2023-05-28 20:15:27 +0500

    lib changed

--- lib/authentication/signin/signin.dart
+++ lib/authentication/signin/signin.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_validators/form_validators.dart';
+import 'controller/signin_controller.dart';
+import '../../components/loading_error.dart';
 
 import '../google_signin/google_signin_button.dart';
 import 'button.dart';
@@ -7,11 +11,21 @@ import 'email.dart';
 import 'forgot_password_button.dart';
 import 'password.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends ConsumerWidget {
   const SignIn({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<SignInState>(signInProvider, (previous, current) {
+      if (current.status.isInProgress) {
+        LoadingSheet.show(context);
+      } else if (current.status.isFailure) {
+        Navigator.of(context).pop();
+        ErrorDialog.show(context, "${current.errorMessage}");
+      } else if (current.status.isSuccess) {
+        Navigator.of(context).pop();
+      }
+    });
     return const Column(
       children: [
         EmailField(),
